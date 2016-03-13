3289a0bef1cb6368e89fb895c65b2d4317573a67
Balazs Scheidler <balazs.scheidler@balabit.com>
2016-03-13 13:07:06 +0100

    cfg: add cfg_load_module() function
    This function can be used to simplify module loading if a GlobalConfig
    instance is already available (e.g.  most of the tests).  This helps
    decoupling the plugin related functions from GlobalConfig, while making the
    call-sites somewhat simpler.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- modules/linux-kmsg-format/tests/test_linux_format_kmsg.c
+++ modules/linux-kmsg-format/tests/test_linux_format_kmsg.c
@@ -174,7 +174,7 @@ void
 init_and_load_kmsgformat_module()
 {
   configuration = cfg_new_snippet(VERSION_VALUE);
-  plugin_load_module("linux-kmsg-format", configuration, NULL);
+  cfg_load_module(configuration, "linux-kmsg-format");
   parse_options.format = "linux-kmsg";
 
   msg_format_options_defaults(&parse_options);
