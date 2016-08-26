76dd34c3e80059e7880591336fc74d6fe31389cc
bkil-syslogng <tamas.nagy@balabit.com>
2016-08-26 15:52:14 +0200

    modules/add-contextual-data: fix warnings
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- modules/add-contextual-data/context-info-db.h
+++ modules/add-contextual-data/context-info-db.h
@@ -33,7 +33,7 @@ typedef void (*ADD_CONTEXT_INFO_CB) (gpointer arg,
                                      const ContextualDataRecord *record);
 
 GList * context_info_db_ordered_selectors(ContextInfoDB *self);
-ContextInfoDB *context_info_db_new();
+ContextInfoDB *context_info_db_new(void);
 void context_info_db_free(ContextInfoDB *self);
 
 ContextInfoDB *context_info_db_ref(ContextInfoDB *self);
