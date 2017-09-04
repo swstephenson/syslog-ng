72abd91ba95dbb27c8726b6e4500d2b437e2770d
Balazs Scheidler <balazs.scheidler@balabit.com>
2017-09-04 13:59:44 +0200

    cfg-lexer: make generators real, auto-loadable plugins
    Instead of registering a block generator into CfgLexer directly, use
    the new Plugin based support code to achieve the same.
    
    This makes generator plugins able to auto-load and also makes the plugin
    startup code less dependant on GlobalConfig.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- modules/confgen/confgen-plugin.c
+++ modules/confgen/confgen-plugin.c
@@ -26,7 +26,6 @@
 #include "cfg-lexer.h"
 #include "messages.h"
 #include "plugin.h"
-#include "plugin-types.h"
 
 #include <string.h>
 #include <errno.h>
@@ -111,7 +110,7 @@ confgen_exec_new(gint context, const gchar *name, const gchar *exec)
 }
 
 gboolean
-confgen_module_init(GlobalConfig *cfg, CfgArgs *args)
+confgen_module_init(PluginContext *plugin_context, CfgArgs *args)
 {
   const gchar *name, *context, *exec;
 
@@ -133,9 +132,7 @@ confgen_module_init(GlobalConfig *cfg, CfgArgs *args)
       msg_error("confgen: exec argument expected");
       return FALSE;
     }
-  cfg_lexer_register_block_generator(cfg->lexer,
-                                     confgen_exec_new(cfg_lexer_lookup_context_type_by_name(context),
-                                                      name, exec));
+  cfg_lexer_register_generator_plugin(plugin_context, confgen_exec_new(cfg_lexer_lookup_context_type_by_name(context), name, exec));
   return TRUE;
 }
 
